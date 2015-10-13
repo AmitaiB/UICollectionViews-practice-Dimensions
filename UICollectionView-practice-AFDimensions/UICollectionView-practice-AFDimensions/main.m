dffa9079d9f3974e44086a8e67b8a7bd6436144e
AmitaiB <blickstein@gmail.com>
2015-10-13 14:34:29 -0400

    Adds Xcode empty project

---
+++ UICollectionView-practice-AFDimensions/UICollectionView-practice-AFDimensions/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UICollectionView-practice-AFDimensions
+//
+//  Created by Amitai Blickstein on 10/13/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
