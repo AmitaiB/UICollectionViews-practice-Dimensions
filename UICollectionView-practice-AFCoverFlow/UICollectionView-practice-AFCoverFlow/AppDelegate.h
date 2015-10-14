24afe274a49779681c02b80c96d0a47d61ae30f1
AmitaiB <blickstein@gmail.com>
2015-10-13 22:08:22 -0400

    Everything SHOULD work, why not

---
+++ UICollectionView-practice-AFCoverFlow/UICollectionView-practice-AFCoverFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionView-practice-AFCoverFlow
+//
+//  Created by Amitai Blickstein on 10/13/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
